68c086e8cdd7af0e3ca3647a19d5802614c3db60
JasimDAce <mohdalijasim786@gmail.com>
2024-06-29 17:19:38 +0530

    Today I have used javaScript

---
+++ condition.js
@@ -0,0 +1,13 @@
+x = 10;
+if(x%2 === 0){
+    console.log('even')
+    something = 'hello';
+    let n = 2;
+}
+else{
+    console.log('odd');
+}
+console.log(something)
+// console.log(n);
+// var if we define in function then it will behave like local variable else global varibale
+console.log( 2 ** 4)
---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello');
+console.log('hi');
+// integer and float are number in js
+// when we dont define variable it will show null
+
+a = 250;
+
+console.log(a);
+console.log(typeof(a));
+
+myName = 'jasim';
+console.log(myName);
+console.log(typeof myName);
+isLogin = true;
+console.log (typeof isLogin);
